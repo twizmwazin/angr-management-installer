cab794a1774a5ce8a6aa8d25389bcf6ab2d3152c
Kevin Phoenix <kevin@kphoenix.us>
2022-08-01 16:56:53 -0700

    Split unzstd into seperate step

--- linux/build_installer.sh
+++ linux/build_installer.sh
@@ -6,8 +6,9 @@ mkdir angr-management-install
 pushd angr-management-install
 
 wget "https://github.com/indygreg/python-build-standalone/releases/download/20220630/cpython-3.10.5+20220630-x86_64_v3-unknown-linux-gnu-pgo+lto-full.tar.zst" -O python.tar.zst
-tar --use-compress-program=unzstd xf python.tar.zst python/install python/licenses
-rm python.tar.zst
+unzstd python.tar.zst
+tar xf python.tar python/install python/licenses
+rm python.tar.zst python.tar
 
 mkdir wheels
 python/install/bin/pip3 download -d wheels angr-management
