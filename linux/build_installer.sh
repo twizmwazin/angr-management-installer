4a709ad861f8f8efa4ff046f29e05755bd924a1a
Kevin Phoenix <kevin@kphoenix.us>
2022-08-01 16:55:03 -0700

    Fix zstd extraction

--- linux/build_installer.sh
+++ linux/build_installer.sh
@@ -6,7 +6,7 @@ mkdir angr-management-install
 pushd angr-management-install
 
 wget "https://github.com/indygreg/python-build-standalone/releases/download/20220630/cpython-3.10.5+20220630-x86_64_v3-unknown-linux-gnu-pgo+lto-full.tar.zst" -O python.tar.zst
-tar xf python.tar.zst python/install python/licenses
+tar --use-compress-program=unzstd xf python.tar.zst python/install python/licenses
 rm python.tar.zst
 
 mkdir wheels
