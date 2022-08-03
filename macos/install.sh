da4b152947907df4ba4c335cf4ee84d81af61d7f
Kevin Phoenix <kevin@kphoenix.us>
2022-08-03 12:03:46 -0700

    Correct removal of quarentine parameter

--- macos/install.sh
+++ macos/install.sh
@@ -7,10 +7,12 @@ cd $(dirname $0)
 
 mkdir ~/.angr-management
 cp -r python ~/.angr-management/python
+xattr -d com.apple.quarantine ~/.angr-management/python/install/lib/libpython3.10.dylib
+xattr -d com.apple.quarantine ~/.angr-management/python/install/bin/python3.10
+
 ~/.angr-management/python/install/bin/pip install ./wheels/*
 cp uninstall.sh ~/.angr-management/
 
-xattr -d com.apple.quarantine ~/.angr-management/python/install/bin/python
 
 # TODO: Make a .app
 
